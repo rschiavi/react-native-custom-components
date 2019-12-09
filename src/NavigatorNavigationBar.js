450cd7fdd4feffbf5abd14e00356d4b8179eeb59
Andrii Uhryn <ugrin14081994@gmail.com>
2019-12-09 14:10:39 +0200

    Fix keys issues.

--- src/NavigatorNavigationBar.js
+++ src/NavigatorNavigationBar.js
@@ -29,11 +29,12 @@ var React = require('react');
 var NavigatorNavigationBarStylesAndroid = require('./NavigatorNavigationBarStylesAndroid');
 var NavigatorNavigationBarStylesIOS = require('./NavigatorNavigationBarStylesIOS');
 import {
-  Platform,
-  StyleSheet,
-  View,
-  ViewPropTypes,
+    Platform,
+    StyleSheet,
+    View,
+    ViewPropTypes,
 } from 'react-native';
+
 var PropTypes = require('prop-types');
 
 var guid = require('./guid');
@@ -43,183 +44,185 @@ var { Map } = require('immutable');
 var COMPONENT_NAMES = ['Title', 'LeftButton', 'RightButton'];
 
 var NavigatorNavigationBarStyles = Platform.OS === 'android' ?
-  NavigatorNavigationBarStylesAndroid : NavigatorNavigationBarStylesIOS;
+    NavigatorNavigationBarStylesAndroid : NavigatorNavigationBarStylesIOS;
 
-var navStatePresentedIndex = function(navState) {
-  if (navState.presentedIndex !== undefined) {
-    return navState.presentedIndex;
-  }
-  // TODO: rename `observedTopOfStack` to `presentedIndex` in `NavigatorIOS`
-  return navState.observedTopOfStack;
+var navStatePresentedIndex = function (navState) {
+    if (navState.presentedIndex !== undefined) {
+        return navState.presentedIndex;
+    }
+    // TODO: rename `observedTopOfStack` to `presentedIndex` in `NavigatorIOS`
+    return navState.observedTopOfStack;
 };
 
 class NavigatorNavigationBar extends React.Component {
-  static propTypes = {
-    navigator: PropTypes.object,
-    routeMapper: PropTypes.shape({
-      Title: PropTypes.func.isRequired,
-      LeftButton: PropTypes.func.isRequired,
-      RightButton: PropTypes.func.isRequired,
-    }).isRequired,
-    navState: PropTypes.shape({
-      routeStack: PropTypes.arrayOf(PropTypes.object),
-      presentedIndex: PropTypes.number,
-    }),
-    navigationStyles: PropTypes.object,
-    style: ViewPropTypes.style,
-  };
-
-  static Styles = NavigatorNavigationBarStyles;
-  static StylesAndroid = NavigatorNavigationBarStylesAndroid;
-  static StylesIOS = NavigatorNavigationBarStylesIOS;
-
-  static defaultProps = {
-    navigationStyles: NavigatorNavigationBarStyles,
-  };
-
-  componentWillMount() {
-    this._reset();
-  }
-
-  /**
-   * Stop transtion, immediately resets the cached state and re-render the
-   * whole view.
-   */
-  immediatelyRefresh = () => {
-    this._reset();
-    this.forceUpdate();
-  };
-
-  _reset = () => {
-    this._key = guid();
-    this._reusableProps = {};
-    this._components = {};
-    this._descriptors = {};
-
-    COMPONENT_NAMES.forEach(componentName => {
-      this._components[componentName] = new Map();
-      this._descriptors[componentName] = new Map();
-    });
-  };
-
-  _getReusableProps = (/*string*/componentName, /*number*/index) => /*object*/ {
-    var propStack = this._reusableProps[componentName];
-    if (!propStack) {
-      propStack = this._reusableProps[componentName] = [];
-    }
-    var props = propStack[index];
-    if (!props) {
-      props = propStack[index] = {style:{}};
-    }
-    return props;
-  };
-
-  _updateIndexProgress = (
-    /*number*/progress,
-    /*number*/index,
-    /*number*/fromIndex,
-    /*number*/toIndex,
-  ) => {
-    var amount = toIndex > fromIndex ? progress : (1 - progress);
-    var oldDistToCenter = index - fromIndex;
-    var newDistToCenter = index - toIndex;
-    var interpolate;
-    if (oldDistToCenter > 0 && newDistToCenter === 0 ||
-        newDistToCenter > 0 && oldDistToCenter === 0) {
-      interpolate = this.props.navigationStyles.Interpolators.RightToCenter;
-    } else if (oldDistToCenter < 0 && newDistToCenter === 0 ||
-               newDistToCenter < 0 && oldDistToCenter === 0) {
-      interpolate = this.props.navigationStyles.Interpolators.CenterToLeft;
-    } else if (oldDistToCenter === newDistToCenter) {
-      interpolate = this.props.navigationStyles.Interpolators.RightToCenter;
-    } else {
-      interpolate = this.props.navigationStyles.Interpolators.RightToLeft;
-    }
-
-    COMPONENT_NAMES.forEach(function (componentName) {
-      var component = this._components[componentName].get(this.props.navState.routeStack[index]);
-      var props = this._getReusableProps(componentName, index);
-      if (component && interpolate[componentName](props.style, amount)) {
-        props.pointerEvents = props.style.opacity === 0 ? 'none' : 'box-none';
-        component.setNativeProps(props);
-      }
-    }, this);
-  };
-
-  updateProgress = (/*number*/progress, /*number*/fromIndex, /*number*/toIndex) => {
-    var max = Math.max(fromIndex, toIndex);
-    var min = Math.min(fromIndex, toIndex);
-    for (var index = min; index <= max; index++) {
-      this._updateIndexProgress(progress, index, fromIndex, toIndex);
-    }
-  };
-
-  render() {
-    var navBarStyle = {
-      height: this.props.navigationStyles.General.TotalNavHeight,
+    static propTypes = {
+        navigator: PropTypes.object,
+        routeMapper: PropTypes.shape({
+            Title: PropTypes.func.isRequired,
+            LeftButton: PropTypes.func.isRequired,
+            RightButton: PropTypes.func.isRequired,
+        }).isRequired,
+        navState: PropTypes.shape({
+            routeStack: PropTypes.arrayOf(PropTypes.object),
+            presentedIndex: PropTypes.number,
+        }),
+        navigationStyles: PropTypes.object,
+        style: ViewPropTypes.style,
     };
-    var navState = this.props.navState;
-    var components = navState.routeStack.map((route, index) =>
-      COMPONENT_NAMES.map(componentName =>
-        this._getComponent(componentName, route, index)
-      )
-    );
 
-    return (
-      <View
-        key={this._key}
-        style={[styles.navBarContainer, navBarStyle, this.props.style]}>
-        {components}
-      </View>
-    );
-  }
+    static Styles = NavigatorNavigationBarStyles;
+    static StylesAndroid = NavigatorNavigationBarStylesAndroid;
+    static StylesIOS = NavigatorNavigationBarStylesIOS;
 
-  _getComponent = (/*string*/componentName, /*object*/route, /*number*/index) => /*?Object*/ {
-    if (this._descriptors[componentName].includes(route)) {
-      return this._descriptors[componentName].get(route);
+    static defaultProps = {
+        navigationStyles: NavigatorNavigationBarStyles,
+    };
+
+    componentWillMount() {
+        this._reset();
     }
 
-    var rendered = null;
+    /**
+     * Stop transtion, immediately resets the cached state and re-render the
+     * whole view.
+     */
+    immediatelyRefresh = () => {
+        this._reset();
+        this.forceUpdate();
+    };
 
-    var content = this.props.routeMapper[componentName](
-      this.props.navState.routeStack[index],
-      this.props.navigator,
-      index,
-      this.props.navState
-    );
-    if (!content) {
-      return null;
+    _reset = () => {
+        this._key = guid();
+        this._reusableProps = {};
+        this._components = {};
+        this._descriptors = {};
+
+        COMPONENT_NAMES.forEach(componentName => {
+            this._components[componentName] = new Map();
+            this._descriptors[componentName] = new Map();
+        });
+    };
+
+    _getReusableProps = (/*string*/componentName, /*number*/index) => /*object*/ {
+        var propStack = this._reusableProps[componentName];
+        if (!propStack) {
+            propStack = this._reusableProps[componentName] = [];
+        }
+        var props = propStack[index];
+        if (!props) {
+            props = propStack[index] = { style: {} };
+        }
+        return props;
+    };
+
+    _updateIndexProgress = (
+        /*number*/progress,
+        /*number*/index,
+        /*number*/fromIndex,
+        /*number*/toIndex,
+    ) => {
+        var amount = toIndex > fromIndex ? progress : (1 - progress);
+        var oldDistToCenter = index - fromIndex;
+        var newDistToCenter = index - toIndex;
+        var interpolate;
+        if (oldDistToCenter > 0 && newDistToCenter === 0 ||
+            newDistToCenter > 0 && oldDistToCenter === 0) {
+            interpolate = this.props.navigationStyles.Interpolators.RightToCenter;
+        } else if (oldDistToCenter < 0 && newDistToCenter === 0 ||
+            newDistToCenter < 0 && oldDistToCenter === 0) {
+            interpolate = this.props.navigationStyles.Interpolators.CenterToLeft;
+        } else if (oldDistToCenter === newDistToCenter) {
+            interpolate = this.props.navigationStyles.Interpolators.RightToCenter;
+        } else {
+            interpolate = this.props.navigationStyles.Interpolators.RightToLeft;
+        }
+
+        COMPONENT_NAMES.forEach(function (componentName) {
+            var component = this._components[componentName].get(this.props.navState.routeStack[index]);
+            var props = this._getReusableProps(componentName, index);
+            if (component && interpolate[componentName](props.style, amount)) {
+                props.pointerEvents = props.style.opacity === 0 ? 'none' : 'box-none';
+                component.setNativeProps(props);
+            }
+        }, this);
+    };
+
+    updateProgress = (/*number*/progress, /*number*/fromIndex, /*number*/toIndex) => {
+        var max = Math.max(fromIndex, toIndex);
+        var min = Math.min(fromIndex, toIndex);
+        for (var index = min; index <= max; index++) {
+            this._updateIndexProgress(progress, index, fromIndex, toIndex);
+        }
+    };
+
+    render() {
+        var navBarStyle = {
+            height: this.props.navigationStyles.General.TotalNavHeight,
+        };
+        var navState = this.props.navState;
+        var components = navState.routeStack.map((route, index) =>
+            COMPONENT_NAMES.map(componentName =>
+                this._getComponent(componentName, route, index),
+            ),
+        );
+
+        return (
+            <View
+                key={this._key}
+                style={[styles.navBarContainer, navBarStyle, this.props.style]}>
+                {components}
+            </View>
+        );
     }
 
-    var componentIsActive = index === navStatePresentedIndex(this.props.navState);
-    var initialStage = componentIsActive ?
-      this.props.navigationStyles.Stages.Center :
-      this.props.navigationStyles.Stages.Left;
-    rendered = (
-      <View
-        ref={(ref) => {
-          this._components[componentName] = this._components[componentName].set(route, ref);
-        }}
-        pointerEvents={componentIsActive ? 'box-none' : 'none'}
-        style={initialStage[componentName]}>
-        {content}
-      </View>
-    );
+    _getComponent = (/*string*/componentName, /*object*/route, /*number*/index) => /*?Object*/ {
+        if (this._descriptors[componentName].includes(route)) {
+            return this._descriptors[componentName].get(route);
+        }
 
-    this._descriptors[componentName] = this._descriptors[componentName].set(route, rendered);
-    return rendered;
-  };
+        var rendered = null;
+
+        var content = this.props.routeMapper[componentName](
+            this.props.navState.routeStack[index],
+            this.props.navigator,
+            index,
+            this.props.navState,
+        );
+        if (!content) {
+            return null;
+        }
+
+        var componentIsActive = index === navStatePresentedIndex(this.props.navState);
+        var initialStage = componentIsActive ?
+            this.props.navigationStyles.Stages.Center :
+            this.props.navigationStyles.Stages.Left;
+        rendered = (
+            <View
+                ref={(ref) => {
+                    this._components[componentName] = this._components[componentName].set(route, ref);
+                }}
+                key={`${componentName}-${route.url}`}
+                style={initialStage[componentName]}
+                pointerEvents={componentIsActive ? 'box-none' : 'none'}
+            >
+                {content}
+            </View>
+        );
+
+        this._descriptors[componentName] = this._descriptors[componentName].set(route, rendered);
+        return rendered;
+    };
 }
 
 
 var styles = StyleSheet.create({
-  navBarContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'transparent',
-  },
+    navBarContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'transparent',
+    },
 });
 
 module.exports = NavigatorNavigationBar;
